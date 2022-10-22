2732f587882134771f9fab215dc3b5626ef06520
ShuVeriDa <bashtarov@outlook.com>
2022-10-22 14:32:59 +0300

    added custom controller and service

---
+++ src/customers/controllers/customers/customers.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { CustomersService } from '../../services/customers/customers.service';
+
+@Controller('customers')
+export class CustomersController {
+  constructor(private customersService: CustomersService) {}
+  @Get()
+  getCustomer() {
+    return this.customersService.findCustomer();
+  }
+}
