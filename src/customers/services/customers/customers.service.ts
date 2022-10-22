ab5dc97fb4d72bbbba4bf0f7c4af18cb58babe2e
ShuVeriDa <bashtarov@outlook.com>
2022-10-22 14:53:17 +0300

    added getCustomer

--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -2,11 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-  findCustomer() {
-    return {
+  users = [
+    {
       id: 1,
       email: 'biltoyn@nakhcho.vu',
       createdAt: new Date(),
-    };
+    },
+    {
+      id: 2,
+      email: '1andoyn@nakhcho.vu',
+      createdAt: new Date(),
+    },
+    {
+      id: 3,
+      email: 'chertoyn@nakhcho.vu',
+      createdAt: new Date(),
+    },
+  ];
+
+  findCustomerById(id: number) {
+    return this.users.find((user) => user.id === id);
   }
 }
