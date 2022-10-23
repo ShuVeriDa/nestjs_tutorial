e985b8f51525f5e12f563dc5a59041d664ffdd97
ShuVeriDa <bashtarov@outlook.com>
2022-10-23 17:08:49 +0300

    connected to PostgreSQL using typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntities } from './typeorm/UserEntities';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '5940530bbbb',
+      database: 'nestjstutorial15series',
+      entities: [UserEntities],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/typeorm/UserEntities.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class UserEntities {
+  @PrimaryGeneratedColumn({
+    type: 'bigint',
+  })
+  id: number;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
+  username: string;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
+  email: string;
+
+  @Column({
+    nullable: false,
+    default: '',
+  })
+  password: string;
+}
