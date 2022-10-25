7d74cd913ee523be51755f9907cc1cbfab94443c
ShuVeriDa <bashtarov@outlook.com>
2022-10-25 13:48:23 +0300

    added Sessions

--- src/auth/controllers/auth/auth.controller.ts
+++ src/auth/controllers/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Request,
+  Get,
+  Session,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
@@ -6,4 +13,12 @@ export class AuthController {
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Request() req) {}
+
+  @Get()
+  async getAuthSession(@Session() session: Record<string, any>) {
+    console.log(session);
+    console.log(session.id);
+    session.authendicated = true;
+    return session;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(
+    session({
+      name: 'NESTJS_SESSION_ID',
+      secret: 'ASASASASASASASFDAFA',
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
   await app.listen(5001);
 }
 bootstrap();
